4760679ee79ab20d90182e272c147d4fc0731196
crypt096 <acvjetan@gmail.com>
2021-03-24 19:26:02 +0100

    Updated code.

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Loading
+const textureLoader = new THREE.TextureLoader()
+
+const normalTexture = textureLoader.load('/textures/NormalMap.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -19,6 +24,7 @@ const sphereGeometry = new THREE.SphereBufferGeometry(.5, 64, 64)
 const material = new THREE.MeshStandardMaterial()
 material.metalness = 0.7;
 material.roughness = 0.2;
+material.normalMap = normalTexture;
 material.color = new THREE.Color(0x292929)
 
 // Mesh
@@ -31,6 +37,21 @@ pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
+
+// Light 2
+
+const pointLight2 = new THREE.PointLight(0xff0000, 2)
+pointLight2.position.set(-1.86, 1, -1.65)
+pointLight2.intensity = 1
+
+scene.add(pointLight2)
+
+// Light 3
+const pointLight3 = new THREE.PointLight(0xe1ff, 2)
+pointLight3.position.set(2.13, -3, -1.98)
+pointLight3.intensity = 6.8
+
+scene.add(pointLight3)
 
 /**
  * Sizes
@@ -82,14 +103,43 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+document.addEventListener('mousemove', onDocumentMouseMove);
+
+let mouseX = 0;
+let mouseY = 0;
+
+let targetX = 0;
+let targetY = 0;
+
+const windowX = window.innerWidth / 2;
+const windowY = window.innerHeight / 2;
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowX)
+    mouseY = (event.clientY - windowY)
+}
+
+const updateSphere = (event) => {
+    sphere.position.y = window.scrollY * .001
+}
+
+window.addEventListener('scroll', updateSphere);
+
 const clock = new THREE.Clock()
 
 const tick = () => {
+
+    targetX = mouseX * .001;
+    targetY = mouseY * .001;
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+
+    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
+    sphere.rotation.x += .05 * (targetY - sphere.rotation.x)
+    sphere.position.z += -.05 * (targetY - sphere.rotation.x)
 
     // Update Orbital Controls
     // controls.update()
